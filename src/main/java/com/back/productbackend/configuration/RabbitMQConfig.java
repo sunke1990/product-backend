dd984894997f48217cfe736bf0fd940eda02d700
sunke <632124845@qq.com>
2022-01-05 17:39:19 +0800

    add RabbitMQConfig

---
+++ src/main/java/com/back/productbackend/configuration/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package com.back.productbackend.configuration;
+
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author sunke
+ * @DATE 2022/1/5
+ **/
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory){
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+}
